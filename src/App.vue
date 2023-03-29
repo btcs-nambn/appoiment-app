0321a83644ce4fed5ebdc00bd055a0fa5b8ce2e9
LinhNguyen <nguyendanglinh270792@gmail.com>
2023-03-29 18:09:06 +0700

    U

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <user-infor></user-infor>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import UserInfor from './components/pages/UserInfor.vue';
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  name: 'AppointmentAppApp',
+  components: {
+    UserInfor,
+  },
+
+  data() {
+    return {
+      
+    };
+  },
+
+  mounted() {
+    
+  },
+
+  methods: {
+    
+  },
+};
+</script>
---
+++ src/components/pages/UserInfor.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+    Hello....
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AppointmentAppUserInfor',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
