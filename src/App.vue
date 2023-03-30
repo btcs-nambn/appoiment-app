54dea523bb1ef52125f78edc663182dff620b55f
LinhNguyen <nguyendanglinh270792@gmail.com>
2023-03-30 08:53:40 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,14 @@
 <template>
   <div>
-    <router-link to="./"></router-link>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-import { RouterLink,RouterView } from 'vue-router';
 
 export default {
 
   components: {
-    RouterLink,
-    RouterView
   },
 }
 </script>
