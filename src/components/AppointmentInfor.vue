6609bd75ede7051c1fc9a40aa2bc939cb8ec4369
LinhNguyen <nguyendanglinh270792@gmail.com>
2023-04-04 14:41:11 +0700

    sửa lại nút và tên hàm

--- src/components/AppointmentInfor.vue
+++ src/components/AppointmentInfor.vue
@@ -1,7 +1,7 @@
 <template>
   <div
-    class="flex justify-center items-center flex-col w-[400px] h-[400px] mt-0 bg-slate-400 z-50 rounded-xl shadow-xl">
-    <h2 class="mt-10 ml-10 text-black">Make Appointment</h2>
+    class="flex justify-center items-center flex-col w-[400px] h-[400px] mt-0 bg-slate-400 z-50 rounded-xl shadow-xl text-white">
+    <h2 class="mt-10 ml-3 text-lg">Make Appointment</h2>
     <form class="mt-3 mr-7" @submit.prevent="addAppointment">
       <label class="ml-12">Title :</label>
       <div class="relative flex">
@@ -18,21 +18,22 @@
         rows="8"
         class="block p-2.5 ml-10 text-xs text-gray-900 bg-gray-50 rounded-xl border border-gray-300 w-[300px] h-[150px] focus:ring-blue-500 focus:border-blue-500 overflow-auto max-h-[150px] min-h-[150px]"
         placeholder="Write your Appointment content here..."
-        v-model="appointmentContent"></textarea>
-      <button
-        class="relative inline-flex items-center justify-center p-0.5 mr-2 overflow-hidden text-xs font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-red-200 via-red-300 to-yellow-200 group-hover:from-red-200 group-hover:via-red-300 group-hover:to-yellow-200 focus:ring-4 focus:outline-none focus:ring-red-100 mt-3 ml-[140px] mb-5"
-        type="submit">
-        <span
-          class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white rounded-md group-hover:bg-opacity-0">
+        v-model="appointmentContent">
+      </textarea>
+      <div class="flex justify-center items-center mt-3 ml-10"> 
+        <button
+          type="button"
+          class="text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+          @click="addAppointment">
           Submit
-        </span>
-      </button>
-      <button
-        class="underline text-sm text-blue-800"
-        type="button"
-        @click="hiddentModal">
-        Hidden
-      </button>
+        </button>
+        <button
+          type="button"
+          class="text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+          @click="$emit('hiden-modal')">
+          Hidden
+        </button>
+      </div>
     </form>
   </div>
 </template>
@@ -61,9 +62,6 @@ export default {
         );
       }
     },
-    hiddentModal() {
-      this.$emit("hiden-modal");
-    }
   },
 };
 </script>
--- src/pages/MakeAppointment.vue
+++ src/pages/MakeAppointment.vue
@@ -19,7 +19,7 @@
       class="flex justify-center items-center z-50 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
       <appointment-infor
         @addAppointment="addAppointment"
-        @hidenModal="show"></appointment-infor>
+        @hidenModal="showModal"></appointment-infor>
     </div>
   </div>
 </template>
@@ -64,7 +64,7 @@ export default {
       this.dateOrder = day.format("DD-MM-YYYY");
       this.timeOrder = time;
     },
-    show() {
+    showModal() {
       this.isVisibleMakeappointment = false;
     },
   },
